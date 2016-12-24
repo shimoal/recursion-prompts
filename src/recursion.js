83f220a8103e0aad6503b691dc9d474c2dd1e632
Alison Reed <alison.michelle.reed@gmail.com>
2016-12-23 17:12:00 -0800

    Pass all tests for sumBelow(#5)

--- src/recursion.js
+++ src/recursion.js
@@ -47,14 +47,20 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+	if (n === 0) {
+		return 0;
+	}
 
+	n = n > 0 ? n - 1 : n + 1;
+
+	return n + sumBelow(n)
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
 
-};
+}
 
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
